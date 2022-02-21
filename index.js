01f6666efa5eaa1db3014929e27d30823699327e
Josette Garcia <josettetgarcia@gmail.com>
2022-02-21 10:39:06 -0600

    commit tree cf469c3a57d9885cc5909f630f6904ad8661cb65

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(word) {    
+  let len = word.length;
+  for (let i = 0; i < (len / 2); i++) {
+      if (word.charAt(i) !== word.charAt(len - 1 - i))
+           return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
-*/
+function accepts a string (or word)
+array = string or word split
+if i[0] matches 1[last] return true and move on 
+if statement loops until all characters are reviewed
+loop exits if any return false 
 
-/*
   Add written explanation of your solution here
-*/
+*/// function takes a string and turns it into an Array
+//
+// We then compare first and last elements of the array 
+// until we meet in the middle (if statements)
+// if any of these bounce back as not matching, we return false
+//if all come back true, exit and return true
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
